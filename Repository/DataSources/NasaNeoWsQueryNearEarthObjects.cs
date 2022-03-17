5189e38d08aa4c4db395ea532d6274be07bb0eda
flopez <flopez@axpe.com>
2022-03-17 23:40:33 +0100

    parse json with culture en-us

--- Repository/DataSources/NasaNeoWsQueryNearEarthObjects.cs
+++ Repository/DataSources/NasaNeoWsQueryNearEarthObjects.cs
@@ -1,15 +1,36 @@
-﻿namespace asteroidalert.Repository
+﻿using System;
+using System.Globalization;
+
+namespace asteroidalert.Repository
 {
     public class NasaNeoWsQueryNearEarthObjects : IQueryNearEarthObjects
     {
         private readonly dynamic _node;
+        private readonly CultureInfo _provider;
         public NasaNeoWsQueryNearEarthObjects(dynamic node)
         {
             _node = node;
+            _provider = new CultureInfo("en-US");
         }
 
-        public double estimated_diameter { get => _node.estimated_diameter.kilometers.estimated_diameter_max; }
+        public double estimated_diameter_max { get => _node.estimated_diameter.kilometers.estimated_diameter_max; }
         public double estimated_diameter_min { get => _node.estimated_diameter.kilometers.estimated_diameter_min; }
         public bool is_potentially_hazardous_asteroid { get => (bool)_node.is_potentially_hazardous_asteroid; }
+        public double estimated_diameter { get => (estimated_diameter_max + estimated_diameter_min) / 2; }
+        public DateTime date
+        {
+            get
+            {
+                return DateTime.Parse(_node.close_approach_data[0]?.close_approach_date, _provider, DateTimeStyles.NoCurrentDateDefault);
+            }
+        }
+        public decimal kilometers_per_hour { 
+            get {
+                string value = _node.close_approach_data[0]?.relative_velocity.kilometers_per_hour;
+                return Convert.ToDecimal(value,_provider);
+            } 
+        } 
+        public string orbiting_body { get => _node.close_approach_data[0]?.orbiting_body; }
+        public string name { get => _node.name; }
     }
 }
--- Repository/IQueryNearEarthObjects.cs
+++ Repository/IQueryNearEarthObjects.cs
@@ -1,4 +1,6 @@
-﻿namespace asteroidalert.Repository
+﻿using System;
+
+namespace asteroidalert.Repository
 {
     /// <summary>
     /// Return information of one NearEarthObject
@@ -6,10 +8,13 @@
     public interface IQueryNearEarthObjects
     {
         public bool is_potentially_hazardous_asteroid { get; }
-
-        public double estimated_diameter { get; }
-
+        public double estimated_diameter_max { get; }
         public double estimated_diameter_min { get; }
+        public double estimated_diameter { get; }
+        public DateTime date { get; }
+        public decimal kilometers_per_hour { get; }
+        public string orbiting_body { get; }
+        public string name { get; }
     }
 
 
